c5b921e61c2fa51950682b46b323a61a4b809a01
Nathan DeSpain <nathandespain@Nathans-MacBook-Pro.local>
2020-06-03 11:29:58 -0700

    Component is almost complete having issues with span not being visible/clicable

--- Article/Article.js
+++ Article/Article.js
@@ -111,3 +111,42 @@ const data = [
 
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 */
+
+function articleMaker(object){
+
+  const article = document.createElement('div')
+  const title = document.createElement('h2')
+  const date = document.createElement('p')
+  const content1 = document.createElement('p')
+  const content2 = document.createElement('p')
+  const content3 = document.createElement('p')
+  const button = document.createElement('span')
+
+  article.appendChild(title)
+  article.appendChild(date)
+  article.appendChild(content1)
+  article.appendChild(content2)
+  article.appendChild(content3)
+  article.appendChild(button)
+
+  article.classList.add('article')
+  article.classList.add('date')
+  button.classList.add('expandButton')
+
+  title.textContent = object.title
+  date.textContent = object.date
+  content1.textContent = object.firstParagraph
+  content2.textContent = object.secondParagraph
+  content3.textContent = object.thirdParagraph
+  button.textContent = 'expand'
+
+  button.addEventListener('click', event=>{
+    article.classList.toggle('article-open')
+  })
+
+  return article
+}
+
+data.forEach(item =>{
+  document.querySelector('.articles').appendChild(articleMaker(item))
+})
